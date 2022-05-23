b0deb56f3a00cd107bb9e9838a787791746dac09
titmouse2797 <anastasmikula@gmail.com>
2022-05-23 14:07:31 +0200

    feat: finish 04 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+let path = require('path');
+let fs = require('fs/promises');
+let dirname = path.join(__dirname, 'files');
+let dirnameCopy = path.join(__dirname, 'files-copy');
 
+fs.rm(dirnameCopy, {
+    recursive: true,
+    force: true
+}).finally(function() {
+    fs.mkdir(dirnameCopy, {
+        recursive: true
+    });
+    fs.readdir(dirname , {
+        withFileTypes: true
+    }).then(function(data) {
+        data.forEach(function(item) {
+            if (item.isFile()) {
+                let pathItem = path.join(dirname, item.name);
+                let pathItemDes = path.join(dirnameCopy, item.name);
+                fs.copyFile(pathItem, pathItemDes);}
+        });
+    });
+});
